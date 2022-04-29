baaa9a2420c5dab96f2af6c0f920fefdc36345f9
BhushanMore3073 <bhushanmore3073@gmail.com>
2022-04-29 14:14:41 +0530

    Phase 5

---
+++ Phase 5/SpringBoot Application (1)/src/main/java/resources/MainController.java
@@ -0,0 +1,13 @@
+package resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index.html";
+  }
+}
